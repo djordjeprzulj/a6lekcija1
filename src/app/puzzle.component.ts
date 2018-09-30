defd071392e89ff1aaf69aaeab425b202054f0bd
djordjeprzulj <przulj@gmail.com>
2018-09-30 21:10:03 +0200

    Dodata obeležja, konstruktor i string interpolacija.

--- src/app/puzzle.component.ts
+++ src/app/puzzle.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-puzzle',
   template: `
-            <h1>Dva lokvanja oko panja.</h1>
-            <p>Glava i uši.</p>`
+            <h1>{{ question }}</h1>
+            <p>{{ solution }}</p>`
 })
 export class PuzzleComponent {
+  question: string;
+  solution: string;
+
+  constructor() {
+    this.question = 'Dva lokvanja oko panja.';
+    this.solution = 'Glava i uši.';
+  }
 }
