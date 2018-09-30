4c4a14511aeb4ec994658dcb30cfa6a29234717e
djordjeprzulj <przulj@gmail.com>
2018-09-30 21:06:49 +0200

    Dodata puzzle komponenta tekstom i rešenjem u template-u.

---
+++ src/app/puzzle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puzzle',
+  template: `
+            <h1>Dva lokvanja oko panja.</h1>
+            <p>Glava i uši.</p>`
+})
+export class PuzzleComponent {
+}
